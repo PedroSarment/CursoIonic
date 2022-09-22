cd999a3fa848ad96499fcc7f27b87f8b9ea4e4c7
Pedro Sarmento <45299871+PedroSarment@users.noreply.github.com>
2022-09-21 22:08:32 -0300

    Cria app para aprender API_REQUEST

---
+++ aprendendo-api-request/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'aprendendo-api-request',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
